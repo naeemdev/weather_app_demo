ccdbc35d75a4dd772c01a8e5f91954cd4fe07ea2
Muhammad Naeem <naeem0313@gmail.com>
2025-08-03 11:49:33 +0500

    Refactor: Remove API_RESPONSE_NULL error type

--- app/src/main/java/com/naeemdev/weather/domain/ErrorType.kt
+++ app/src/main/java/com/naeemdev/weather/domain/ErrorType.kt
@@ -8,7 +8,6 @@ enum class ErrorType(val errorMessageResId: Int) {
     FORBIDDEN(R.string.error_forbidden),
     UNAUTHORIZED(R.string.error_unauthorized),
     UNKNOWN(R.string.error_unknown),
-    API_RESPONSE_NULL(R.string.error_no_data),
     INTERNAL_SERVER_ERROR(R.string.error_internal_server),
     IO_EXCEPTION(R.string.error_io_exception),
     EMPTY_RESPONSE(R.string.sorry_no_data_found),
