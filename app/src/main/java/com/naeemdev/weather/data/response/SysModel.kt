bb65b04ee2a52f11c0909a9a0ecb824e4ee39d2c
Muhammad Naeem <naeem0313@gmail.com>
2025-08-03 11:51:27 +0500

    Refactor: Remove SysModel and SysModelD

--- app/src/main/java/com/naeemdev/weather/data/response/SysModel.kt
+++
@@ -1,9 +0,0 @@
-package com.naeemdev.weather.data.response
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class SysModel(
-    val sunrise: Long,
-    val sunset: Long,
-)
--- app/src/main/java/com/naeemdev/weather/domain/model/SysModelD.kt
+++
@@ -1,6 +0,0 @@
-package com.naeemdev.weather.domain.model
-
-data class SysModelD(
-    val sunrise: Long,
-    val sunset: Long,
-)
